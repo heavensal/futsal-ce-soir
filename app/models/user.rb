d6a255c256b21fff5f754163c743bc434ef58d3c
adam lamouri <adamlamouri.pro@gmail.com>
2023-11-13 00:21:05 +0100

    user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  private
+
+  def method_name
+
+  end
 end
